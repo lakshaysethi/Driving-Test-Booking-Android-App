cb1392cdc2d0c8948a47481f8eaf0155bc1722c9
lakshaysethi <45325278+lakshaysethi@users.noreply.github.com>
2020-04-08 23:32:33 +1200

    work on Main Activity mostly done

--- app/src/main/java/com/lakshaysethi/driverlicencetestbooking/MainActivity.java
+++ app/src/main/java/com/lakshaysethi/driverlicencetestbooking/MainActivity.java
@@ -7,19 +7,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     public static ArrayList<Pojoclasses.Slot> slotsList= new ArrayList<Pojoclasses.Slot>();
-    public static ArrayList<Pojoclasses.User> users = new ArrayList<Pojoclasses.User>();
+    public static ArrayList<Pojoclasses.User> usersStaticList = new ArrayList<Pojoclasses.User>();
 
     public static Pojoclasses.User currentUser;//Need to populate on click of main activity
 
     private Button continueButton;
     private EditText licenceInputText;
-    private String licenceString;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,22 +30,54 @@ public class MainActivity extends AppCompatActivity {
         populateUsersArrayListFromDatabase();
         populateSlotsArrayListFromDatabase();
 
-
+        //get reference to the widgets on the screen
         licenceInputText = findViewById(R.id.licenceInput);
+        continueButton =(Button) findViewById(R.id.continueButton);
 
 
 
-        continueButton =(Button) findViewById(R.id.bookButton);
         continueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                licenceString =  licenceInputText.toString();
-                currentUser = new Pojoclasses.User(licenceString);
+
+
+                authenticateUser();
+
                 openBookingActivity();
+
+
+
+
             }
         });
 
+
+
     }
+//Important Functions below
+    private void authenticateUser() {
+        for(Pojoclasses.User user : usersStaticList ){
+            if (user.licenceNumber.equals(licenceInputText.getText().toString())){
+                Toast.makeText(this,"Welcome Back! :)",Toast.LENGTH_LONG);
+
+            }
+        }
+        Pojoclasses.User newUser = new Pojoclasses.User(licenceInputText.getText().toString());
+        usersStaticList.add(newUser);
+        saveUserStaticLisToDatabase();
+        Toast.makeText(this,"Welcome New User! you have been added to the Database :)",Toast.LENGTH_LONG);
+    }
+
+    private void saveUserStaticLisToDatabase() {
+        //TODO
+    }
+
+    public void openBookingActivity(){
+        Intent intent = new Intent(this,BookSlot.class);
+        //intent.putExtra("licenceNumber",licenceString);
+        startActivity(intent);
+    }
+
 
     private void populateSlotsArrayListFromDatabase() {
         //TODO
@@ -55,11 +88,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
 //
-    public void openBookingActivity(){
-        Intent intent = new Intent(this,BookSlot.class);
-        //intent.putExtra("licenceNumber",licenceString);
-        startActivity(intent);
-    }
+
 }
 
 
