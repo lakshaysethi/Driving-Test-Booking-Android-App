3ff9102de2f842e478056c59dd68810f5c239b2a
lakshaysethi <45325278+lakshaysethi@users.noreply.github.com>
2020-04-10 04:59:54 +1200

    added stuff to the activity

--- app/src/main/java/com/lakshaysethi/driverlicencetestbooking/MatrixActivity.java
+++ app/src/main/java/com/lakshaysethi/driverlicencetestbooking/MatrixActivity.java
@@ -12,21 +12,51 @@ public class MatrixActivity extends AppCompatActivity {
 
 
 
-    RecyclerView date1RecyclerView= (RecyclerView) findViewById(R.id.dateRecyclerView);
-    RecyclerView date2RecyclerView= (RecyclerView) findViewById(R.id.date2RecyclerView);
-    RecyclerView date3RecyclerView= (RecyclerView) findViewById(R.id.date3RecyclerView);
-    RecyclerView date4RecyclerView= (RecyclerView) findViewById(R.id.date4RecyclerView);
-    RecyclerView date5RecyclerView= (RecyclerView) findViewById(R.id.date5RecyclerView);
+    RecyclerView date1RecyclerView;
+    RecyclerView date2RecyclerView;
+    RecyclerView date3RecyclerView;
+    RecyclerView date4RecyclerView;
+    RecyclerView date5RecyclerView;
+    RecyclerView rvLdates;
 
-    ArrayList<RecyclerView> rViewArrayList = new ArrayList<>();
+    ArrayList<RecyclerView> rViewArrayList = new ArrayList<RecyclerView>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_matrix);
+        date1RecyclerView= (RecyclerView) findViewById(R.id.dateRecyclerView);
+        date2RecyclerView= (RecyclerView) findViewById(R.id.date2RecyclerView);
+        date3RecyclerView= (RecyclerView) findViewById(R.id.date3RecyclerView);
+        date4RecyclerView= (RecyclerView) findViewById(R.id.date4RecyclerView);
+        date5RecyclerView= (RecyclerView) findViewById(R.id.date5RecyclerView);
+        rViewArrayList.add(date1RecyclerView);
+        rViewArrayList.add(date2RecyclerView);
+        rViewArrayList.add(date3RecyclerView);
+        rViewArrayList.add(date4RecyclerView);
+        rViewArrayList.add(date5RecyclerView);
+        int count = 0;
+        for(RecyclerView rv : rViewArrayList ){
+            rv.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false));
+            ArrayList<Pojoclasses.Slot> tmp = new ArrayList<Pojoclasses.Slot>(Controller.slotsList.subList(count,count+4));
+            rv.setAdapter(new SlotAdapter(tmp,this,R.layout.slot_button));
+            count +=5;
+        }
+        rvLdates = (RecyclerView) findViewById(R.id.date6RecyclerView);
+        rvLdates.setLayoutManager(new LinearLayoutManager(this));
 
-        date1RecyclerView.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false));
-        date1RecyclerView.setAdapter(new SlotAdapter(Controller.slotsList,this));
+        rvLdates.setAdapter(new SlotAdapter(getArrayListOfUniqueDates(Controller.slotsList),this,R.layout.date_and_day_view));
+    }
+
+    private ArrayList<Pojoclasses.Slot> getArrayListOfUniqueDates(ArrayList<Pojoclasses.Slot> slotsList) {
+        ArrayList<Pojoclasses.Slot> al = new ArrayList<Pojoclasses.Slot>();
+        int count=0;
+        for(int i=0;i<5;i++){
+            al.add(slotsList.get(count));
+            count+=8;
+        }
+        return  al;
 
     }
 }
