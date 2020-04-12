19a59c5a978f249e40d84f211a707eb3b09370a0
lakshaysethi <45325278+lakshaysethi@users.noreply.github.com>
2020-04-12 13:35:34 +1200

    Create AllFunctionsUnitTest.java

---
+++ app/src/test/java/com/lakshaysethi/driverlicencetestbooking/AllFunctionsUnitTest.java
@@ -0,0 +1,18 @@
+package com.lakshaysethi.driverlicencetestbooking;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+import static org.junit.Assert.*;
+
+public class AllFunctionsUnitTest {
+
+
+
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
